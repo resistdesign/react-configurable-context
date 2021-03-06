3e45a5fae30a546eaeb08987d0274852ec3e0a6e
Ryan Graff <ryantgraff@resistdesign.com>
2021-03-06 16:37:22 -0500

    feat: (Package) Add the basis for ConfigurableContext.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,50 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import React, { Context, createContext, FC, PropsWithChildren, useContext, useState } from 'react';
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
-}
+export type BaseContextType = Record<any, any>;
 
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+export type ConfigurableContextSetter<ContextType extends BaseContextType> = (value: ContextType) => void;
+
+export type ConfigurableContextContextType<ContextType extends BaseContextType> = Map<
+  Context<ContextType>,
+  ConfigurableContextSetter<ContextType>
+>;
+
+export const ConfigurableContextContext = createContext<ConfigurableContextContextType<BaseContextType> | undefined>(
+  undefined
+);
+
+export const {
+  Consumer: ConfigurableContextContextConsumer,
+  Provider: ConfigurableContextContextProvider,
+} = ConfigurableContextContext;
+
+export type ConfigurableContextProps<ContextType extends BaseContextType> = {
+  context: Context<ContextType>;
+  defaultValue: ContextType | undefined;
+};
+
+export const ConfigurableContext: FC<ConfigurableContextProps<BaseContextType>> = <
+  ContextType extends BaseContextType
+>({
+  context,
+  defaultValue,
+  children,
+}: PropsWithChildren<ContextType>) => {
+  const { Provider } = context;
+  const configurableContextMap: ConfigurableContextContextType<ContextType> =
+    useContext<ConfigurableContextContextType<ContextType>>(
+      ConfigurableContextContext as Context<ConfigurableContextContextType<ContextType>>
+    ) || new Map<Context<ContextType>, ConfigurableContextSetter<ContextType>>();
+  const [value, setValue] = useState<ContextType | undefined>(
+    typeof defaultValue === 'undefined' ? useContext<ContextType>(context) : defaultValue
+  );
+  const contextValueSetter = configurableContextMap.get(context) || setValue;
+
+  configurableContextMap.set(context, contextValueSetter);
+
+  return (
+    <Provider value={value}>
+      <ConfigurableContextContextProvider value={configurableContextMap}>{children}</ConfigurableContextContextProvider>
+    </Provider>
+  );
 };
