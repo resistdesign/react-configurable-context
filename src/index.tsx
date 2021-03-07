5530ca8e90c2ebf851e0549b62959c13108521b3
Ryan Graff <ryantgraff@resistdesign.com>
2021-03-06 22:48:28 -0500

    feat: (index) Add the basis for Config and Setting.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
-import React, { Context, createContext, FC, PropsWithChildren, useContext, useState } from 'react';
+import React, {
+  Context,
+  createContext,
+  FC,
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
 export type BaseContextType = Record<any, any>;
 
 export type ConfigurableContextSetter<ContextType extends BaseContextType> = (value: ContextType) => void;
+export type ConfigurableContextPropSetter<ContextType extends BaseContextType> = (
+  name: keyof ContextType,
+  value: ContextType[keyof ContextType]
+) => void;
 
 export type ConfigurableContextContextType<ContextType extends BaseContextType> = Map<
   Context<ContextType>,
@@ -12,11 +25,7 @@ export type ConfigurableContextContextType<ContextType extends BaseContextType> 
 export const ConfigurableContextContext = createContext<ConfigurableContextContextType<BaseContextType> | undefined>(
   undefined
 );
-
-export const {
-  Consumer: ConfigurableContextContextConsumer,
-  Provider: ConfigurableContextContextProvider,
-} = ConfigurableContextContext;
+const { Provider: ConfigurableContextContextProvider } = ConfigurableContextContext;
 
 export type ConfigurableContextProps<ContextType extends BaseContextType> = {
   context: Context<ContextType>;
@@ -47,4 +56,54 @@ export const ConfigurableContext: FC<ConfigurableContextProps<BaseContextType>> 
       <ConfigurableContextContextProvider value={configurableContextMap}>{children}</ConfigurableContextContextProvider>
     </Provider>
   );
+};
+
+export default ConfigurableContext;
+
+export type ConfigProps<ContextType extends BaseContextType> = {
+  context: Context<ContextType>;
+};
+
+const ConfigContext = createContext<ConfigurableContextPropSetter<BaseContextType> | undefined>(undefined);
+const { Provider: ConfigProvider } = ConfigContext;
+
+export const Config: FC<ConfigProps<BaseContextType>> = <ContextType extends BaseContextType>({
+  context,
+  children,
+}: PropsWithChildren<ContextType>) => {
+  const configurableContextMap: ConfigurableContextContextType<ContextType> = useContext<
+    ConfigurableContextContextType<ContextType>
+  >(ConfigurableContextContext as Context<ConfigurableContextContextType<ContextType>>);
+  const currentValue = useContext<ContextType>(context);
+  const setter = configurableContextMap ? configurableContextMap.get(context) : undefined;
+  const propSetter = useCallback<ConfigurableContextPropSetter<ContextType>>(
+    (name, value) => {
+      if (setter) {
+        setter({
+          ...currentValue,
+          [name]: value,
+        });
+      }
+    },
+    [setter, currentValue]
+  );
+
+  return <ConfigProvider value={propSetter}>{children}</ConfigProvider>;
+};
+
+export type SettingProps = {
+  name: string;
+  children: any;
+};
+
+export const Setting: FC<SettingProps> = ({ name, children }) => {
+  const propSetter = useContext<ConfigurableContextPropSetter<any> | undefined>(ConfigContext);
+
+  useEffect(() => {
+    if (propSetter) {
+      propSetter(name, children);
+    }
+  }, [propSetter, name, children]);
+
+  return null;
 };
